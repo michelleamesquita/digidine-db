08c6f9aa9e4a389cd2c3f22e4d25430460ea925a
michelleamesquita <44561732+michelleamesquita@users.noreply.github.com>
2024-12-10 08:12:13 -0300

    Update documentdb.tf

--- documentdb.tf
+++ documentdb.tf
@@ -3,7 +3,10 @@ resource "aws_docdb_cluster" "docdb_cluster" {
   cluster_identifier       = "digidine-docdb-cluster"
   master_username          = "admin"
   master_password          = "Digidine2024!2024"
-  vpc_security_group_ids   = [aws_security_group.docdb_sg.id]
+  
+  # Usando o grupo de segurança existente
+  vpc_security_group_ids   = ["sg-0b07368d446355324"]
+  
   db_subnet_group_name     = aws_docdb_subnet_group.docdb_subnet_group.name
   engine_version           = "4.0.0"
   backup_retention_period  = 7
@@ -15,28 +18,9 @@ resource "aws_docdb_cluster" "docdb_cluster" {
 resource "aws_docdb_subnet_group" "docdb_subnet_group" {
   name        = "digidine-docdb-subnet-group"
   description = "Subnet group for DocumentDB cluster"
-  subnet_ids  = ["subnet-0ed2012a602007132", "subnet-0b94c50396a512ce1", "subnet-02ab00ddbcaf406e8"]
-}
 
-# Security Group para DocumentDB
-resource "aws_security_group" "docdb_sg" {
-  name        = "digidine-docdb-sg"
-  description = "Security group for DocumentDB cluster"
-  vpc_id      = "vpc-028adccd04bfc371d"
-
-  ingress {
-    from_port   = 27017
-    to_port     = 27017
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  # Subnets corrigidas para a mesma VPC
+  subnet_ids  = ["subnet-0c5488876c338133f", "subnet-0c8680c9b686d74ea", "subnet-0eed11c225de65036"]
 }
 
 # DocumentDB Instance
